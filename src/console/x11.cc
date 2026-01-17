0d603837709e4368fe163779dcd354fafa585223
art yerkes <art.yerkes@gmail.com>
2026-01-16 22:08:36 -0800

    Try without acceleration if acceleration fails

--- src/console/x11.cc
+++ src/console/x11.cc
@@ -383,6 +383,9 @@ struct fb_window *x11_fb_init(int xsize, int ysize, char *name,
   );
   fbwin->window_id = SDL_GetWindowID(fbwin->x11_fb_window);
   fbwin->x11_fb_render = SDL_CreateRenderer(fbwin->x11_fb_window, -1, SDL_RENDERER_ACCELERATED);
+  if (!fbwin->x11_fb_render) {
+  	fbwin->x11_fb_render = SDL_CreateRenderer(fbwin->x11_fb_window, -1, 0);
+  }
 
 	/*  Make sure the window is mapped:  */
 	/*  Fill the ximage with black pixels:  */

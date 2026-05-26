b1b69d5f469bbae03e770e320c45957382f688f5
Cacodemon345 <wahil1976@outlook.com>
2026-05-27 04:31:17 +0600

    Revert some overzealous changes

--- src/promemul/dreamcast_scramble.cc
+++ src/promemul/dreamcast_scramble.cc
@@ -19,12 +19,12 @@
 
 static unsigned int seed;
 
-void my_srandom(unsigned int n)
+void my_srand(unsigned int n)
 {
   seed = n & 0xffff;
 }
 
-unsigned int my_random(void)
+unsigned int my_rand(void)
 {
   seed = (seed * 2109 + 9273) & 0x7fff;
   return (seed + 0xc000) & 0xffff;
@@ -55,7 +55,7 @@ void load_chunk(FILE *fh, unsigned char *ptr, unsigned long sz)
   for(i = sz-1; i >= 0; --i)
     {
       /* Select a replacement index */
-      int x = (my_random() * i) >> 16;
+      int x = (my_rand() * i) >> 16;
 
       /* Swap */
       int tmp = idx[i];
@@ -71,7 +71,7 @@ void load_file(FILE *fh, unsigned char *ptr, unsigned long filesz)
 {
   unsigned long chunksz;
 
-  my_srandom(filesz);
+  my_srand(filesz);
 
   /* Descramble 2 meg blocks for as long as possible, then
      gradually reduce the window down to 32 bytes (1 slice) */
@@ -142,7 +142,7 @@ void save_chunk(FILE *fh, unsigned char *ptr, unsigned long sz)
   for(i = sz-1; i >= 0; --i)
     {
       /* Select a replacement index */
-      int x = (my_random() * i) >> 16;
+      int x = (my_rand() * i) >> 16;
 
       /* Swap */
       int tmp = idx[i];
@@ -158,7 +158,7 @@ void save_file(FILE *fh, unsigned char *ptr, unsigned long filesz)
 {
   unsigned long chunksz;
 
-  my_srandom(filesz);
+  my_srand(filesz);
 
   /* Descramble 2 meg blocks for as long as possible, then
      gradually reduce the window down to 32 bytes (1 slice) */
